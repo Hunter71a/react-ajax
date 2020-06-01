cce383d91b79f323a835d0cdce1b2c121da903ac
Carl Olson <colson9@student.cscc.edu>
2020-06-01 17:30:39 -0400

    add connection to jsonplaceholder.cypress.io/posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,52 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.cypress.io/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Chump'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+                console.log(this.state.posts);
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />;
+        });
+
         return (
             <div>
                 <section className="Posts">
+                    {posts}
                     <Post />
                     <Post />
                     <Post />
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
@@ -24,5 +61,7 @@ class Blog extends Component {
         );
     }
 }
+
+//console.log('Blog did start');
 
 export default Blog;
